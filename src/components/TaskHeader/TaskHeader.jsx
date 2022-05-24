4fae0f4e8c63afd1f2db3828a218508b7968caf9
Akash Mondal <102036593+IAkashMondal@users.noreply.github.com>
2022-05-24 10:47:47 +0530

    Add files via upload

--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -3,16 +3,16 @@ import styles from "./taskHeader.module.css";
 
 const TaskHeader = ({userdata,completed}) => {
   // sample values to be replaced
-  // remaning & Total task------------------------------------------>
+  // unCompletedTask & Total task------------------------------------------>
   let totalTask = userdata.length;
-  let Remaning = completed.length;
+  let unCompletedTask = completed.length;
   // NOTE: do not delete `data-cy` key value pair
 
 
   return (
     <div data-cy="task-header" className={styles.taskHeader}>
       <h1 className={styles.task_pera_head} >Todo List</h1>
-      <div className={styles.task_center}><h3 data-cy="header-remaining-task">you have {Remaning} of </h3>
+      <div className={styles.task_center}><h3 data-cy="header-remaining-task">you have {unCompletedTask} of </h3>
        <h3 data-cy="header-total-task"> {totalTask} tasks remanig</h3>
        </div>
     </div>
